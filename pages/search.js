52182cc28fec8dc9aefefbadb1f240ed7b011f6a
Jason <jdclaeys@outlook.com>
2022-05-17 22:50:07 -0500

    add search component

--- pages/search.js
+++ pages/search.js
@@ -1,9 +1,80 @@
+import React, { useState } from 'react'
+import  Link  from 'next/link'
+import ListItem from '../components/listItem'
+
 const SearchPage = () => {
+  const notes = JSON.parse(localStorage.getItem('notes'))
+  const [searchNotes, setSearchNotes] = useState(JSON.parse(localStorage.getItem('notes')))
+  const [term, setTerm] = useState('')
+
+  const titleInput = document.querySelector('#title')
+  const bodyInput = document.querySelector('#body')
+
+  const onClick = e => {
+    e.preventDefault()
+
+    if (term === '') {
+      setSearchNotes([-2])
+    }
+
+    if (titleInput.checked && bodyInput.checked) {
+      setSearchNotes(notes.filter(note => note.title.toLowerCase().includes(term) || note.body.toLowerCase().includes(term)))
+    } else if (titleInput.checked) {
+      setSearchNotes(notes.filter(note => note.title.toLowerCase().includes(term)))
+    } else if (bodyInput.checked) {
+      setSearchNotes(notes.filter(note => note.body.toLowerCase().includes(term)))
+    } else if (!titleInput.checked && !bodyInput.checked) {
+      setSearchNotes([-1])
+    }
+  }
+
+  const renderedList = searchNotes.map(note => {
+    return <ListItem note={note} key={note.id} />
+  })
+
   return (
     <>
-      <h1 className="text-4xl font-bold">
-        Search Page
-      </h1>
+      <h1>Search Notes</h1>
+      <form>
+        <label>
+          <span className='label-body'>Search by title</span>
+          <input type="checkbox" id="title" />
+        </label>
+        <label htmlFor="body">
+          <span className='label-body'>Search by content</span>
+          <input type="checkbox" id="body" />
+        </label>
+        <label htmlFor="search">
+          <strong>Search term</strong>
+        </label>
+        <input 
+          type="text" 
+          id="search" 
+          placeholder="Enter search term..."
+          value={term}
+          onChange={e => setTerm(e.target.value)}
+        />
+        <div id="buttons">
+          <button 
+            id="search-btn"
+            onClick={onClick}
+          >
+            Search
+          </button>
+        </div>
+      </form>
+      <div>
+        {searchNotes[0] === -1 ? 
+          <p>Please choose to search by title, body, or both.</p>
+        : searchNotes[0] === -2 ? 
+          <p>Please enter a search term.</p> 
+        : searchNotes.length === 0 ? 
+        <div>
+          <p>You have no notes.</p>
+          <p>Create one <Link href='/'><a>here</a></Link></p>
+        </div>
+        : renderedList}
+      </div>
     </>
   )
 }
